3848fc36cb5c08bdd1e8623da675faebf47fb007
Penthious <tleffew1994@gamil.com>
2016-09-01 15:28:53 -0600

    reformated code to abide by eslint

--- resources/assets/js/Search.jsx
+++ resources/assets/js/Search.jsx
@@ -33,9 +33,7 @@ class Search extends Component {
                         .filter((show) => `${show.title} ${show.description}`
                             .toUpperCase()
                             .indexOf(this.state.searchTerm.toUpperCase()) >= 0
-                        ).map((show) => (
-                                <ShowCard {...show} key={show.imdbID} />
-                            )
+                        ).map((show) => <ShowCard {...show} key={show.imdbID} />
                         )}
                 </div>
             </div>
